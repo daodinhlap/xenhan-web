92692288d7cde5116b2d22b620af03424e9b5012
Nhu Dinh Thuan <nhudinhthuan@Nhus-MacBook-Pro.local>
2017-10-05 14:44:56 +0700

    add

--- xenhan-shop-webui/src/main/java/com/homedirect/xenhan/shop/controller/ShopController.java
+++ xenhan-shop-webui/src/main/java/com/homedirect/xenhan/shop/controller/ShopController.java
@@ -1,5 +1,8 @@
 package com.homedirect.xenhan.shop.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.homedirect.repo.model.response.RepositoryResponse;
 import com.homedirect.xenhan.model.AttributeConfig;
 import com.homedirect.xenhan.model.Shop;
-import com.homedirect.xenhan.model.common.request.XnUserInforRequest;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  * author: hieunv - hieu.nguyen2@homedirect.com.vn
@@ -60,5 +60,23 @@ public class ShopController extends AbstractController {
     } catch (Exception e) {
       return entity.getBody().getMessage().getBytes();
     }
+  }
+  
+  @GetMapping(value = "/thong-tin-shop")
+  public ModelAndView showShop(HttpServletRequest httpRequest, HttpSession session) {
+      ModelAndView mv = new ModelAndView("shop.detail");
+      mv.addObject("title","Xe Nhàn - Thông Tin Shop");
+      String url = apiExchangeService.createUrlWithToken(httpRequest, "shop", "get-shop");
+      url += "&shop-name=" + session.getAttribute(AttributeConfig.SHOPNAME);
+      logger.info("url " + url);
+      TypeReference<RepositoryResponse<Shop>> reference = new TypeReference<RepositoryResponse<Shop>>() {};
+      try {
+        RepositoryResponse<Shop> entity = apiExchangeService.get(httpRequest, url, reference);
+        mv.addObject("shop", entity.getData());
+      } catch (Exception e) {
+        logger.error(e.getMessage(), e);
+        mv.addObject("error", e.getMessage());
+      }
+      return mv;
   }
 }
